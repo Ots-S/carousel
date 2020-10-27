44d5c8e828c4f0f336ca66f842fa08c3f9a47c2a
Seb Ots <ots.sebastien@gmail.com>
2020-10-27 19:14:15 +0100

    feat - carousel

---
+++ carousel.js
@@ -0,0 +1,62 @@
+const data = [
+  { name: "adidas", image: "http://62.210.247.201:9000/adidas.png", weight: 5 },
+  {
+    name: "bhfitness",
+    image: "http://62.210.247.201:9000/bhfitness.png",
+    weight: 3,
+  },
+  { name: "domyos", image: "http://62.210.247.201:9000/domyos.png", weight: 5 },
+  {
+    name: "kettler",
+    image: "http://62.210.247.201:9000/kettler.png",
+    weight: 2,
+  },
+  { name: "reebok", image: "http://62.210.247.201:9000/reebok.png", weight: 3 },
+  {
+    name: "tunturi",
+    image: "http://62.210.247.201:9000/tunturi.png",
+    weight: 3,
+  },
+  { name: "zycle", image: "http://62.210.247.201:9000/zycle.png", weight: 1 },
+  { name: "BKOOL", image: "http://62.210.247.201:9000/BKOOL.png", weight: 3 },
+  { name: "BTWIN", image: "http://62.210.247.201:9000/BTWIN.png", weight: 5 },
+  {
+    name: "fitshow",
+    image: "http://62.210.247.201:9000/fitshow.png",
+    weight: 2,
+  },
+  { name: "Tacx", image: "http://62.210.247.201:9000/Tacx.png", weight: 2 },
+  {
+    name: "klarfit",
+    image: "http://62.210.247.201:9000/klarfit.png",
+    weight: 3,
+  },
+  { name: "Hammer", image: "http://62.210.247.201:9000/Hammer.png", weight: 2 },
+];
+
+const numberOfImages = 7;
+const indexes = [];
+for (let i = 0; i <= numberOfImages - 1; i++) {
+  indexes.push(i);
+}
+console.log(indexes);
+const time = 1000;
+
+function checkAndIncrement(index) {
+  if (index < data.length - 1) {
+    return index + 1;
+  } else {
+    return 0;
+  }
+}
+
+function changeImg() {
+  for (let i = 0; i <= indexes.length - 1; i++) {
+    document.getElementById("slide" + i).src = data[indexes[i]].image;
+  }
+  for (let i = 0; i <= indexes.length - 1; i++) {
+    indexes[i] = checkAndIncrement(indexes[i]);
+  }
+  setTimeout("changeImg()", time);
+}
+window.onload = changeImg;
